2520ff1ff7fd77bcdb151a57791cdba936575365
chizhcode <91279744+chizhcode@users.noreply.github.com>
2025-05-30 15:59:57 +0300

    format

--- lua/plugins/qol/conform.lua
+++ lua/plugins/qol/conform.lua
@@ -1,55 +1,41 @@
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
-    lazy = false,
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
     keys = {
       {
         '<leader>f',
         function()
-          require('conform').format { async = true, lsp_fallback = true }
+          require('conform').format { async = true, lsp_format = 'fallback' }
         end,
         mode = '',
         desc = '[F]ormat buffer',
       },
     },
     opts = {
-      notify_on_error = true,
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
-    --format_on_save = function(bufnr)
-    --  -- Disable "format_on_save lsp_fallback" for languages that don't
-    --  -- have a well standardized coding style. You can add additional
-    --  -- languages here or re-enable it for the disabled ones.
-    --  local disable_filetypes = { c = true, cpp = true }
-    --  return {
-    --    timeout_ms = 500,
-    --    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-    --  }
-    --end,
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
+        end
+      end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        sh = { 'shfmt' },
-        html = { 'djlint' },
-        go = { 'gofmt', 'goimports' },
-        c = { 'clang_format' },
-        cpp = { 'clang_format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
-      },
-      formatters = {
-        clang_format = {
-          prepend_args = { '--style=file', '--fallback-style=LLVM' },
-        },
-        shfmt = {
-          prepend_args = { '-i', '4' },
-        },
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
