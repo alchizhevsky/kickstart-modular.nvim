7c94cc981ba050ca0526d60393d602c54205b4a2
alchizhevsky <>
2025-10-08 04:48:02 +0300

    ignore lazy-lock

--- lua/plugins/appearance/noice.lua
+++ lua/plugins/appearance/noice.lua
@@ -1,51 +1,45 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  opts = {
-    -- add any options here
-  },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     'MunifTanjim/nui.nvim',
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     'rcarriga/nvim-notify',
   },
-  require('noice').setup {
-    routes = {
-      {
-        -- Match messages containing "Copilot"
-        filter = { find = 'Copilot' },
-        opts = { skip = true }, -- skip this message entirely
+  config = function()
+    require('noice').setup {
+      routes = {
+        {
+          filter = { find = 'Copilot' },
+          opts = { skip = true },
+        },
       },
-    },
-    views = {
-      cmdline_popup = {
-        position = { row = 5, col = '50%' },
-        size = { width = 60, height = 'auto' },
-        border = { style = 'rounded', padding = { 0, 1 } },
-        win_options = {
-          winblend = 0,
-          winhighlight = {
-            Normal = 'Normal',
-            FloatBorder = 'DiagnosticInfo',
+      views = {
+        cmdline_popup = {
+          position = { row = 5, col = '50%' },
+          size = { width = 60, height = 'auto' },
+          border = { style = 'rounded', padding = { 0, 1 } },
+          win_options = {
+            winblend = 0,
+            winhighlight = {
+              Normal = 'Normal',
+              FloatBorder = 'DiagnosticInfo',
+            },
+          },
+        },
+        popupmenu = {
+          relative = 'editor',
+          position = { row = 8, col = '50%' },
+          size = { width = 60, height = 10 },
+          border = { style = 'rounded', padding = { 0, 1 } },
+          win_options = {
+            winblend = 0,
+            winhighlight = {
+              Normal = 'Normal',
+              FloatBorder = 'DiagnosticInfo',
+            },
           },
         },
       },
-      popupmenu = {
-        relative = 'editor',
-        position = { row = 8, col = '50%' },
-        size = { width = 60, height = 10 },
-        border = { style = 'rounded', padding = { 0, 1 } },
-        win_options = {
-          winblend = 0,
-          winhighlight = {
-            Normal = 'Normal',
-            FloatBorder = 'DiagnosticInfo',
-          },
-        },
-      },
-    },
-  },
+    }
+  end,
 }
