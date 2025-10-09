dd03cbe3200ad175683d8b7f1f92c577dfc8e0a2
alchizhevsky <>
2025-10-09 18:25:29 +0300

    noice cmdline bug fixed

--- lua/plugins/appearance/noice.lua
+++ lua/plugins/appearance/noice.lua
@@ -7,24 +7,30 @@ return {
   },
   config = function()
     require('noice').setup {
-      routes = {
-        {
-          filter = { find = 'Copilot' },
-          opts = { skip = true },
-        },
+      cmdline = {
+        enabled = true,
+        view = 'cmdline_popup', -- centered popup
       },
       views = {
         cmdline_popup = {
           position = { row = 5, col = '50%' },
           size = { width = 60, height = 'auto' },
           border = { style = 'rounded', padding = { 0, 1 } },
-          win_options = {
-            winblend = 0,
-            winhighlight = {
-              Normal = 'Normal',
-              FloatBorder = 'DiagnosticInfo',
-            },
-          },
+        },
+        cmdline_popupmenu = {
+          relative = 'editor',
+          position = { row = 8, col = '50%' },
+          size = { width = 60, height = 10 },
+          border = { style = 'rounded', padding = { 0, 1 } },
+        },
+      },
+
+      -- 👇 These two lines are the important ones
+      throttle = 100, -- smooth out redraw bursts in 0.11+
+      routes = {
+        {
+          filter = { event = 'cmdline_hide' }, -- ignore the premature hide
+          opts = { skip = true },
         },
       },
     }
